96bc7149896a552712170599e58576fa2ab535f3
Rodolpho Henrique <rodolphosnow@gmail.com>
2022-11-21 12:08:15 -0300

    feat: included fail test for command_center_controller_spec

--- spec/controllers/comunication_controller_spec.rb
+++ spec/controllers/comunication_controller_spec.rb
@@ -4,12 +4,18 @@ RSpec.describe "CommunicationController",type: :request do
 
   describe "POST run_commands" do
     let(:tempfile) { fixture_file_upload('test/files/command.txt') }
+    let(:invalid_tempfile) { fixture_file_upload('test/files/invalid_command.txt') }
 
-    it "assigns send the commands to command center" do
+    it "Assign send the commands to command center" do
       post "/comunication", params: { file: tempfile }
       
       expect(JSON.parse(response.body)).to eq({"data"=>"1 3 N\n5 1 E\n", "status"=>200})
-      expect(assigns(:teams)).to eq([team])
+    end
+
+    it "Does not assign send the commands to command center with invalid data" do
+      post "/comunication", params: { file: invalid_tempfile }
+      
+      expect(JSON.parse(response.body)).to eq({"data"=>"error, invalid file", "status"=>422})
     end
   end
 end
